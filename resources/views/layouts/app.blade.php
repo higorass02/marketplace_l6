f13a5c4da77181439e6e172f65aeab11581278ec
higor santos <higoraugusto120@gmail.com>
2021-06-29 00:49:23 -0300

    refatorando pequenas mudancas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,9 @@
                     <div class="my-2 my-lg-0">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
+                                <span class="nav-link">{{ auth()->user()->name }}</span>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="#" onclick="event.preventDefault(); document.querySelector('form.logout').submit();">Sair</a>
                                 <form action="{{ route('logout') }}" class="logout" method="post" style="display: none;">
                                     @csrf
